ab86b20694950b8cddd7704b2528af7890036e50
Joel Kapuku <joelkap2001@yahoo.fr>
2018-01-09 21:34:45 +0200

    added folders and some js files

---
+++ LE_SEMEUR_NPO/src/LE_SEMEUR_NPO/wwwroot/App/config.route.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('config', [
+        // Angular modules 
+        'ngRoute'
+
+        // Custom modules 
+
+        // 3rd Party Modules
+        
+    ]);
+})();
